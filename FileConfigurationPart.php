e881bc3a2c9b622d2056bc93d3995c1a380eaff5
Billy Visto <bvisto@gustavus.edu>
2014-03-27 12:38:32 -0500

    Added class for representing individual pieces of a file for modifications

---
+++ FileConfigurationPart.php
@@ -0,0 +1,315 @@
+<?php
+/**
+ * @package CMS
+ * @author  Billy Visto
+ */
+
+namespace Gustavus\CMS;
+use InvalidArgumentException,
+  PHPParser_Node_Expr_Array,
+  PHPParser_Parser,
+  PHPParser_NodeTraverser,
+  PHPParser_PrettyPrinter_Default,
+  PHPParser_Lexer;
+
+/**
+ * Object representing an individual piece of the file configuration
+ *
+ * @package CMS
+ * @author  Billy Visto
+ */
+class FileConfigurationPart
+{
+  /**
+   * The type of the content
+   * @var string
+   */
+  private $contentType;
+
+  /**
+   * The current content
+   * @var string
+   */
+  private $content;
+
+  /**
+   * The current key in the configuration array
+   * @var integer
+   */
+  private $key;
+
+  /**
+   * Parsed php nodes
+   *
+   * @var string
+   */
+  private $phpNodes;
+
+  /**
+   * Editable php nodes
+   *
+   * @var array Array of nodes keyed by their index.
+   */
+  private $editablePHPNodeValues = [];
+
+  /**
+   * Original values
+   *
+   * @var array|string Array for php content type. String otherwise
+   */
+  private $valuesBeforeEdit = [];
+
+  /**
+   * Flag to designate that this item has been edited
+   *
+   * @var boolean
+   */
+  private $edited = false;
+
+  /**
+   * Sets up the object
+   *
+   * @param array $params Params to populate the object with
+   */
+  public function __construct(array $params = array())
+  {
+    if (!isset($params['contentType'], $params['content'], $params['key'])) {
+      throw new InvalidArgumentException('The expected keys of contentType, content, and key were not found.');
+    }
+
+    $this->contentType = $params['contentType'];
+    $this->content     = $params['content'];
+    $this->key         = $params['key'];
+  }
+
+  /**
+   * Checks to see if the current content type is a php type
+   *
+   * @return boolean
+   */
+  private function isPHPContent()
+  {
+    return $this->contentType === Config::PHP_CONTENT_TYPE;
+  }
+
+  /**
+   * Gets the content type
+   *
+   * @return string
+   */
+  public function getContentType()
+  {
+    return $this->contentType;
+  }
+
+  /**
+   * Gets the key
+   *
+   * @return integer
+   */
+  public function getKey()
+  {
+    return $this->key;
+  }
+
+  /**
+   * Gets the content
+   *
+   * @return string
+   */
+  public function getContent($wrapEditableContent = false)
+  {
+    if ($this->isPHPContent()) {
+      if (Config::ALLOW_PHP_EDITS && ($wrapEditableContent || $this->edited)) {
+        $prettyPrinter = new GustavusPrettyPrinter;
+        $this->buildEditablePHPNodes($wrapEditableContent);
+        return str_replace('    ', '  ', $prettyPrinter->prettyPrint($this->phpNodes));
+      }
+      return $this->content;
+    }
+
+    // not php if we are here.
+    if ($wrapEditableContent) {
+      return $this->wrapEditableContent($this->content);
+    }
+    return $this->content;
+  }
+
+  /**
+   * Parses php content
+   *
+   * @return array Array of parsed nodes
+   */
+  private function parseContent()
+  {
+    if (!$this->isPHPContent()) {
+      return false;
+    }
+    $parser = new PHPParser_Parser(new PHPParser_Lexer);
+
+    return $parser->parse(sprintf('<?php %s ?>', $this->content));
+  }
+
+  /**
+   * Builds php configuration parts.
+   *   Sets $this->phpNodes to an array of PHPParser nodes or null if not php content
+   *
+   * @return void
+   */
+  private function buildPHPNodes()
+  {
+    if (!$this->isPHPContent()) {
+      $this->phpNodes = null;
+    } else {
+      if (!isset($this->phpNodes)) {
+        $phpNodes = $this->parseContent();
+        $traverser = new PHPParser_NodeTraverser;
+        $this->phpNodes = $traverser->traverse($phpNodes);
+      }
+    }
+  }
+
+  /**
+   * Gets phpNodes. Builds them if they don't exist.
+   *
+   * @return array|null Array of PHPParser nodes or null if not php content
+   */
+  private function getPHPNodes()
+  {
+    if (!isset($this->phpNodes)) {
+      $this->phpNodes = $this->buildPHPNodes();
+    }
+    return $this->phpNodes;
+  }
+
+  /**
+   * Wraps editable content into a span with class of editable. Span includes data attribute of data-index to identify itself.
+   *
+   * @param  string $content content to wrap
+   * @param  integer $subKey  sub key off of the current key
+   * @param  integer $subSubKey  sub key off of the sub key
+   * @return string
+   */
+  private function wrapEditableContent($content, $subKey = null, $subSubKey = null)
+  {
+    $index = $this->buildEditableIndex($subKey, $subSubKey);
+    return sprintf('<div class="editable" data-index="%s">%s</div>', $index, trim($content));
+  }
+
+  /**
+   * Builds the index for the specified key combo
+   *
+   * @param  integer $subKey  sub key off of the current key
+   * @param  integer $subSubKey  sub key off of the sub key
+   * @return string
+   */
+  private function buildEditableIndex($subKey = null, $subSubKey = null)
+  {
+    $index = (string) $this->key;
+    if ($subKey !== null) {
+      $index .= '-' . $subKey;
+    }
+    if ($subSubKey !== null) {
+      $index .= '-' . $subSubKey;
+    }
+    return $index;
+  }
+
+  /**
+   * Builds editable phpNodes. Wraps their values in a span with class of editable
+   *
+   * @param  boolean $wrapEditable Whether we want to wrap editable nodes or not.
+   * @return void
+   */
+  private function buildEditablePHPNodes($wrapEditable = false)
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      return;
+    }
+    if (!isset($this->phpNodes)) {
+        $this->buildPHPNodes();
+    }
+    foreach ($this->phpNodes as $key => &$node) {
+      if (in_array($node->getType(), Config::$editablePHPNodeTypes)) {
+        // we can edit this node
+        if ($node->expr instanceof PHPParser_Node_Expr_Array) {
+          // we need to look into the array to find our values
+          foreach ($node->expr->items as $subKey => &$item) {
+            if (in_array($item->value->getType(), Config::$editablePHPExprTypes)) {
+              // add this to our editable array.
+              $this->editablePHPNodeValues[$this->buildEditableIndex($key, $subKey)] = &$item->value->value;
+
+              if ($wrapEditable) {
+                // wrap our value for editing
+                $item->value->value = $this->wrapEditableContent($item->value->value, $key, $subKey);
+              }
+            }
+          }
+         // $this->buildPHPNodePart($node->expr);
+        } else if (in_array($node->expr->getType(), Config::$editablePHPExprTypes)) {
+          // we can edit this expr
+          $this->editablePHPNodeValues[$this->buildEditableIndex($key)] = &$node->expr->value;
+
+          if ($wrapEditable) {
+            // wrap our value for editing
+            $node->expr->value = $this->wrapEditableContent($node->expr->value, $key);
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * Edits the node's value specified by $index with $newContent.
+   *
+   * @param  string $index      Identifier for this node
+   * @param  string $newContent Content to replace with
+   * @return boolean True on success. False on failure.
+   */
+  public function editValue($index, $newContent)
+  {
+    if (!$this->isPHPContent()) {
+      // save our original value before editing
+      $this->valuesBeforeEdit = $this->content;
+      // non php content is editable
+      $this->content = $newContent;
+      $this->edited = true;
+      return true;
+    }
+
+    if (!Config::ALLOW_PHP_EDITS) {
+      return false;
+    }
+
+    // make sure our editable nodes are built.
+    if (empty($this->editablePHPNodeValues)) {
+      $this->buildEditablePHPNodes();
+    }
+    if (isset($this->editablePHPNodeValues[$index])) {
+
+      // save our original value before editing.
+      $this->valuesBeforeEdit[$index] = $this->editablePHPNodeValues[$index];
+      // edit
+      $this->editablePHPNodeValues[$index] = $newContent;
+      $this->edited = true;
+      return true;
+    } else {
+      // this node doesn't appear to be editable.
+      // @todo Throw exception or display a message saying that this node isn't editable.
+      return false;
+    }
+  }
+
+  /**
+   * Gets the original values for items that have been edited
+   *
+   * @return array|string|null Array for php content edits. String otherwise. Null if nothing has been edited
+   */
+  public function getValueBeforeEdit()
+  {
+    if (empty($this->valuesBeforeEdit)) {
+      return null;
+    }
+    return $this->valuesBeforeEdit;
+  }
+}
---
+++ Test/FileConfigurationPartTest.php
@@ -0,0 +1,484 @@
+<?php
+/**
+ * @package  CMS
+ * @subpackage Test
+ * @author  Billy Visto
+ */
+
+namespace Gustavus\CMS\Test;
+
+use Gustavus\CMS\Config,
+  Gustavus\CMS\FileConfigurationPart,
+  Gustavus\Test\TestObject;
+
+/**
+ * Class to test FileConfigurationPart
+ *
+ * @package  CMS
+ * @subpackage Test
+ * @author  Billy Visto
+ */
+class FileConfigurationPartTest extends TestBase
+{
+  /**
+   * Part to use for testing
+   *
+   * @var FileConfigurationPart
+   */
+  private $part;
+
+  /**
+   * Basic params for building a FileContentPart representing normal content
+   * @var array
+   */
+  private $partParams = [
+    'contentType' => Config::OTHER_CONTENT_TYPE,
+    'content' => 'test content',
+    'key'   => 0,
+  ];
+
+  /**
+   * Basic params for buiulding a FileContentPart representing php content
+   * @var array
+   */
+  private $PHPPartParams = [
+    'contentType' => Config::PHP_CONTENT_TYPE,
+    'content' => '$config[\'content\'] = \'some content\';',
+    'key'   => 0,
+  ];
+
+  public function setUp()
+  {
+  }
+
+  public function tearDown()
+  {
+  }
+
+  /**
+   * Builds a basic FileConfigurationPart from partParams
+   * @return void
+   */
+  private function buildFileConfigurationPart()
+  {
+    $this->part = new TestObject(new FileConfigurationPart($this->partParams));
+  }
+
+  /**
+   * Builds a basic FileConfigurationPart from PHPPartParams
+   * @return void
+   */
+  private function buildPHPFileConfigurationPart()
+  {
+    $this->part = new TestObject(new FileConfigurationPart($this->PHPPartParams));
+  }
+
+  /**
+   * @test
+   */
+  public function testConstructor()
+  {
+    $this->buildFileConfigurationPart();
+
+    $this->assertSame($this->partParams['contentType'], $this->part->contentType);
+    $this->assertSame($this->partParams['content'], $this->part->content);
+    $this->assertSame($this->partParams['key'], $this->part->getKey());
+  }
+
+  /**
+   * @test
+   * @expectedException \InvalidArgumentException
+   */
+  public function testName()
+  {
+    $part = new FileConfigurationPart;
+  }
+
+  /**
+   * @test
+   */
+  public function isPHPContent()
+  {
+    $this->buildPHPFileConfigurationPart();
+
+    $this->assertTrue($this->part->isPHPContent());
+  }
+
+  /**
+   * @test
+   */
+  public function isPHPContentFalse()
+  {
+    $this->buildFileConfigurationPart();
+
+    $this->assertFalse($this->part->isPHPContent());
+  }
+
+  /**
+   * @test
+   */
+  public function getContent()
+  {
+    $this->buildFileConfigurationPart();
+
+    $this->assertSame($this->partParams['content'], $this->part->getContent());
+  }
+
+  /**
+   * @test
+   */
+  public function getContentPHP()
+  {
+    $this->buildPHPFileConfigurationPart();
+
+    $this->assertSame($this->PHPPartParams['content'], $this->part->getContent());
+  }
+
+  /**
+   * @test
+   */
+  public function getContentEditable()
+  {
+    $this->buildFileConfigurationPart();
+
+    $actual = $this->part->getContent(true);
+    $this->assertContains($this->partParams['content'], $actual);
+    $this->assertContains($this->wrappedEditableIdentifier, $actual);
+  }
+
+  /**
+   * @test
+   */
+  public function getContentPHPEditable()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $editablePiece = 'some content';
+    $content = sprintf('$config[\'content\'] = \'%s\';', $editablePiece);
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => $content,
+      'key'   => 0,
+    ];
+    $part = new FileConfigurationPart($params);
+
+    $actual = $part->getContent(true);
+    $this->assertContains($editablePiece, $actual);
+    $this->assertContains($this->wrappedEditableIdentifier, $actual);
+    $this->assertContains('></span', str_replace($editablePiece, '', $actual));
+  }
+
+  /**
+   * @test
+   */
+  public function getContentNotEditable()
+  {
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => '$config[\'content\'] = someFunctionCall();',
+      'key'   => 0,
+    ];
+    $part = new FileConfigurationPart($params);
+
+    $actual = $part->getContent(true);
+    $this->assertNotContains($this->wrappedEditableIdentifier, $actual);
+  }
+
+  /**
+   * @test
+   */
+  public function parseContentNotPHP()
+  {
+    $this->buildFileConfigurationPart();
+
+    $this->assertFalse($this->part->parseContent());
+  }
+
+  /**
+   * @test
+   */
+  public function partContent()
+  {
+    $content = '$config = [\'content\' => \'some content\'];';
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => $content,
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $result = $part->parseContent();
+    $this->assertTrue(is_array($result));
+  }
+
+  /**
+   * @test
+   */
+  public function buildPHPNodesNotPHP()
+  {
+    $this->buildFileConfigurationPart();
+
+    $this->assertNull($this->part->buildPHPNodes());
+  }
+
+  /**
+   * @test
+   */
+  public function buildPHPNodes()
+  {
+    $content = '$config = [\'content\' => \'some content\'];';
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => $content,
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $part->buildPHPNodes();
+    $actual = $part->phpNodes;
+    $this->assertNotNull($actual);
+    $this->assertTrue(is_array($actual));
+    $this->assertSame(1, count($actual));
+  }
+
+  /**
+   * @test
+   */
+  public function getPHPNodesNotPHP()
+  {
+    $this->buildFileConfigurationPart();
+
+    $this->assertNull($this->part->getPHPNodes());
+  }
+
+  /**
+   * @test
+   */
+  public function getAndBuildPHPNodesFromTestIndex()
+  {
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => self::$indexConfigArray['phpcontent'][0],
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $this->assertEmpty($part->phpNodes);
+    $part->buildPHPNodes();
+    $actual = $part->phpNodes;
+    $this->assertNotNull($actual);
+    $this->assertTrue(is_array($actual));
+    $this->assertSame(4, count($actual));
+  }
+
+  /**
+   * @test
+   * @dataProvider buildEditableIndexData
+   */
+  public function buildEditableIndex($expected, $key, $subKey = null, $subSubKey = null)
+  {
+    $this->partParams['key'] = $key;
+    $this->buildFileConfigurationPart();
+    $actual = $this->part->buildEditableIndex($subKey, $subSubKey);
+    $this->assertSame($expected, $actual);
+  }
+
+  /**
+   * DataProvider for buildEditableIndex
+   *
+   * @return array
+   */
+  public function buildEditableIndexData()
+  {
+    return [
+      ['1', 1],
+      ['2-0', 2, 0],
+      ['1-1-1', 1, 1, 1],
+    ];
+  }
+
+  /**
+   * @test
+   */
+  public function buildEditablePHPNodes()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => self::$indexConfigArray['phpcontent'][0],
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $this->assertEmpty($part->editablePHPNodeValues);
+
+    $part->buildEditablePHPNodes();
+    $this->assertSame(4, count($part->phpNodes));
+
+    // There are 3 sub parts that are editable from the first node.
+    $this->assertSame(3, count($part->editablePHPNodeValues));
+
+    $expected = [
+      '0-0-0' => 'Some Title',
+      '0-0-1' => 'Some Sub Title',
+      '0-0-2' => 'This is some content.',
+    ];
+    $this->assertSame($expected, $part->editablePHPNodeValues);
+  }
+
+  /**
+   * @test
+   */
+  public function buildEditablePHPNodesAddingEditablePiece()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => self::$indexConfigArray['phpcontent'][0] . '$title = \'Another title\';',
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $this->assertEmpty($part->editablePHPNodeValues);
+
+    $part->buildEditablePHPNodes();
+    $this->assertSame(5, count($part->phpNodes));
+
+    // There are 3 sub parts that are editable from the first node, and 1 from the last.
+    $this->assertSame(4, count($part->editablePHPNodeValues));
+
+    $expected = [
+      '0-0-0' => 'Some Title',
+      '0-0-1' => 'Some Sub Title',
+      '0-0-2' => 'This is some content.',
+      '0-4'   => 'Another title',
+    ];
+    $this->assertSame($expected, $part->editablePHPNodeValues);
+  }
+
+  /**
+   * @test
+   */
+  public function buildEditablePHPNodesWrapEditable()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => self::$indexConfigArray['phpcontent'][0],
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $this->assertEmpty($part->editablePHPNodeValues);
+
+    $part->buildEditablePHPNodes(true);
+    $this->assertSame(4, count($part->phpNodes));
+
+    // There are 3 sub parts that are editable from the first node.
+    $this->assertSame(3, count($part->editablePHPNodeValues));
+
+    $expected = [
+      '0-0-0' => 'Some Title',
+      '0-0-1' => 'Some Sub Title',
+      '0-0-2' => 'This is some content.',
+    ];
+
+    $this->assertSame(array_keys($expected), array_keys($part->editablePHPNodeValues));
+
+    foreach ($expected as $key => $value) {
+      // make sure it has our wrapper
+      $this->assertContains($this->wrappedEditableIdentifier, $part->editablePHPNodeValues[$key]);
+      // make sure that wrapper includes the key
+      $this->assertContains($key, $part->editablePHPNodeValues[$key]);
+    }
+  }
+
+  /**
+   * @test
+   */
+  public function editValuePHP()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $editablePiece = 'some content';
+    $content = sprintf('$config[\'content\'] = \'%s\';', $editablePiece);
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => $content,
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $part->buildEditablePHPNodes();
+    $this->assertSame($editablePiece, $part->editablePHPNodeValues['0-0']);
+
+    $edit = $editablePiece . ' additions';
+    $this->assertTrue($part->editValue('0-0', $edit));
+    $this->assertSame($part->editablePHPNodeValues['0-0'], $edit);
+
+    $expected = sprintf('$config[\'content\'] = \'%s\';', $edit);
+    $this->assertSame($expected , $part->getContent());
+  }
+
+  /**
+   * @test
+   */
+  public function editValuePHPChangingMultipleValues()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $params = [
+      'contentType' => Config::PHP_CONTENT_TYPE,
+      'content' => self::$indexConfigArray['phpcontent'][0],
+      'key'   => 0,
+    ];
+    $part = new TestObject(new FileConfigurationPart($params));
+
+    $editablePieces = [
+      '0-0-0' => 'Some Title',
+      '0-0-1' => 'Some Sub Title',
+      '0-0-2' => 'This is some content.',
+    ];
+
+    // @todo replace this when we are supporting the ability to edit php.
+    $this->assertTrue($part->editValue('0-0-1', 'A new sub title'));
+    $this->assertTrue($part->editValue('0-0-2', '<p>Test content.</p>'));
+
+    $editedContent = $part->getContent();
+
+    // file_put_contents(self::$testFileDir . 'index.php', $editedContent);
+
+    // var_dump($editedContent);
+  }
+
+  /**
+   * @test
+   */
+  public function editValueHTML()
+  {
+    $this->buildFileConfigurationPart();
+    $actual = $this->part->editValue('0', 'brand new content');
+
+    $this->assertTrue($actual);
+    $this->assertSame('brand new content', $this->part->getContent());
+    $this->assertSame($this->partParams['content'], $this->part->getValueBeforeEdit());
+  }
+
+  /**
+   * @test
+   */
+  public function getValueBeforeEdit()
+  {
+    $this->buildFileConfigurationPart();
+    $this->assertNull($this->part->getValueBeforeEdit());
+  }
+}
