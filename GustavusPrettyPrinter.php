a7cbba966c41710f4c59c768cd7124608e658db6
Billy Visto <bvisto@gustavus.edu>
2014-03-27 12:49:19 -0500

    Added custom PrettyPrinter for PHPParser

---
+++ GustavusPrettyPrinter.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * @package  CMS
+ * @author  Billy Visto
+ */
+
+namespace Gustavus\CMS;
+use PHPParser_PrettyPrinter_Default,
+  PHPParser_Node_Expr_Array,
+  //PHPParser_Node_Stmt_Function,
+  PHPParser_Node_Stmt_Break;
+
+/**
+ * Adds our coding standard formating to the default PrettyPrinter
+ *
+ * @todo  Come back to this once PHPParser has support for whitespace.
+ *
+ * @package  CMS
+ * @author  Billy Visto
+ */
+class GustavusPrettyPrinter extends PHPParser_PrettyPrinter_Default
+{
+  /**
+   * {@inheritdoc}
+   */
+  public function pExpr_Array(PHPParser_Node_Expr_Array $node) {
+    return "[\n" . str_replace(';', ',', $this->pStmts($node->items)) . "\n]";
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function pComments(array $comments) {
+    $result = '';
+
+    foreach ($comments as $comment) {
+      if ($comment instanceof \PHPParser_Comment_Doc) {
+        // doc block comment
+        // add a new line before it.
+        $result .= "\n" . $comment->getReformattedText() . "\n";
+      } else {
+        // normal comment like this.
+        $result .= $comment->getReformattedText() . "\n";
+      }
+    }
+
+    return $result;
+  }
+
+  protected function pStmts(array $nodes, $indent = true) {
+    $pNodes = array();
+     foreach ($nodes as $node) {
+        // var_dump(get_class($node));
+        // var_dump($node->subNodes);
+        // var_dump($node->attributes);
+        // var_dump($node->getAttribute('comments'));
+         $pNodes[] = $this->pComments($node->getAttribute('comments', array()))
+                   . $this->p($node)
+                   . ($node instanceof PHPParser_Node_Expr ? ';' : '');
+     }
+
+     if ($indent) {
+         return '    ' . preg_replace(
+             '~\n(?!$|' . $this->noIndentToken . ')~',
+             "\n" . '    ',
+             implode("\n", $pNodes)
+         );
+     } else {
+         return implode("\n", $pNodes);
+     }
+  }
+}
---
+++ Test/GustavusPrettyPrinterTest.php
@@ -0,0 +1,236 @@
+<?php
+/**
+ * @package  CMS
+ * @subpackage Test
+ * @author  Billy Visto
+ */
+
+namespace Gustavus\CMS\Test;
+
+use PHPParser_Node_Expr_Array,
+  PHPParser_Parser,
+  PHPParser_NodeTraverser,
+  PHPParser_Lexer,
+  Gustavus\CMS\GustavusPrettyPrinter,
+  Gustavus\CMS\Config;
+
+/**
+ * Class to test FileConfigurationPart
+ *
+ * @package  CMS
+ * @subpackage Test
+ * @author  Billy Visto
+ */
+class GustavusPrettyPrinterTest extends TestBase
+{
+  public function setUp()
+  {
+  }
+
+  public function tearDown()
+  {
+  }
+  /**
+   * @test
+   */
+  public function editValueChangingMultipleValues1()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $content = '
+/**
+ * [executeSomeContent description]
+ * @return [type] [description]
+ */
+function executeSomeContent()
+{
+  return \'This is some executed content.\';
+  if (true) {
+    // true
+    echo \'hello\';
+
+    if (false) {
+      // @todo
+      // false
+    }
+  }
+}
+';
+
+    $parser = new PHPParser_Parser(new PHPParser_Lexer);
+    $nodes = $parser->parse(sprintf('<?php %s ?>', $content));
+
+    $traverser = new PHPParser_NodeTraverser;
+    $phpNodes = $traverser->traverse($nodes);
+
+    $prettyPrinter = new GustavusPrettyPrinter;
+
+    $actual = str_replace('    ', '  ', $prettyPrinter->prettyPrint($phpNodes));
+
+
+file_put_contents(self::$testFileDir . 'index.php', $content);
+    file_put_contents(self::$testFileDir . 'actual.php', $actual);
+    exit;
+    $this->assertSame($expected, $actual);
+  }
+
+  /**
+   * @test
+   */
+  public function editValueChangingMultipleValues()
+  {
+    if (!Config::ALLOW_PHP_EDITS) {
+      $this->markTestSkipped('PHP is not editable');
+    }
+    $content = '// use template getter...
+// must use $config[\'templatepreference\']
+$test = \'arst\';
+$config = [
+  \'title\' => \'Some Title\',
+  \'subTitle\' => \'Some Sub Title\',
+  \'content\' => \'This is some content.\',
+  \'content\' => "$test",
+  \'localNav\' => [
+    \'arst\',
+    \'arst\'
+  ],
+];
+
+$config[\'content\'] .= executeSomeContent();
+
+/**
+ * [executeSomeContent description]
+ * @return [type] [description]
+ */
+function executeSomeContent()
+{
+  return \'This is some executed content.\';
+  if (true) {
+    echo \'hello\';
+
+    if (false) {
+      /*test*/
+      // false
+      // test
+    }
+  }
+}
+
+/**
+ * Test class
+ */
+class test
+{
+  /**
+   * [$val description]
+   * @var [type]
+   */
+  private $val;
+
+  /**
+   * [arst description]
+   * @param  [type] $value [description]
+   * @return [type]        [description]
+   */
+  public function arst($value)
+  {
+    $value = $value;
+    foreach ($value as $val => $key) {
+      $val = (int) $key;
+    }
+
+    switch($value) {
+      case \'arst\':
+        /*arst*/
+        $arst = \'arst\';
+          break;
+      default:
+        $art = \'arst\';
+    }
+  }
+}
+
+ob_start();';
+
+    $parser = new PHPParser_Parser(new PHPParser_Lexer);
+    $nodes = $parser->parse(sprintf('<?php %s ?>', $content));
+
+    $traverser = new PHPParser_NodeTraverser;
+    $phpNodes = $traverser->traverse($nodes);
+
+    $prettyPrinter = new GustavusPrettyPrinter;
+
+    $actual = str_replace('    ', '  ', $prettyPrinter->prettyPrint($phpNodes));
+
+    $expected = '// use template getter...
+// must use $config[\'templatepreference\']
+$test = \'arst\';
+$config = [
+  \'title\' => \'Some Title\',
+  \'subTitle\' => \'Some Sub Title\',
+  \'content\' => \'This is some content.\',
+  \'content\' => "{$test}",
+  \'localNav\' => [
+    \'arst\',
+    \'arst\',
+  ],
+];
+$config[\'content\'] .= executeSomeContent();
+
+/**
+ * [executeSomeContent description]
+ * @return [type] [description]
+ */
+function executeSomeContent()
+{
+  return \'This is some executed content.\';
+  if (true) {
+    echo \'hello\';
+    if (false) {
+      // false
+    }
+  }
+}
+
+/**
+ * Test class
+ */
+class test
+{
+
+  /**
+   * [$val description]
+   * @var [type]
+   */
+  private $val;
+
+  /**
+   * [arst description]
+   * @param  [type] $value [description]
+   * @return [type]    [description]
+   */
+  public function arst($value)
+  {
+    $value = $value;
+    foreach ($value as $val => $key) {
+      $val = (int) $key;
+    }
+    switch ($value) {
+      case \'arst\':
+        /*arst*/
+        $arst = \'arst\';
+        break;
+      default:
+        $art = \'arst\';
+    }
+  }
+}
+ob_start();';
+file_put_contents(self::$testFileDir . 'index.php', $content);
+    file_put_contents(self::$testFileDir . 'actual.php', $actual);
+    exit;
+    $this->assertSame($expected, $actual);
+
+  }
+}
