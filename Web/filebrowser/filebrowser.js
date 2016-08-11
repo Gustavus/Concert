0fc97355a398a9ace19aa148211e3e05b8776180
Billy Visto <bvisto@gustavus.edu>
2016-08-11 15:13:51 -0500

    Fixed filebrowser plugin not using the right callback

--- Web/filebrowser/filebrowser.js
+++ Web/filebrowser/filebrowser.js
@@ -1,9 +1,9 @@
 tinymce.PluginManager.add("filebrowser", function(editor){
   // save our original file browser callback
-  editor.settings.origFileBrowserCallback = editor.settings.file_browser_callback;
-  // hijack the file_browser_callback
-  editor.settings.file_browser_callback = function(fieldName, url, type, win) {
-    if (type === 'file') {
+  editor.settings.origFilePickerCallback = editor.settings.file_picker_callback;
+  // hijack the file_picker_callback
+  editor.settings.file_picker_callback = function(callback, value, meta) {
+    if (meta.filetype && meta.filetype === 'file') {
       // we only want to use our custom filebrowser for file selections and not media
       editor.windowManager.open({
         title: 'Choose a page',
@@ -12,7 +12,7 @@ tinymce.PluginManager.add("filebrowser", function(editor){
             text: 'Concert File Manager',
             onclick: function() {
               editor.windowManager.close();
-              return editor.settings.origFileBrowserCallback(fieldName, url, type, win);
+              return editor.settings.origFilePickerCallback(callback, value, meta);
             },
             icon: 'browse',
             classes: 'primary'
@@ -21,21 +21,12 @@ tinymce.PluginManager.add("filebrowser", function(editor){
         setURL: function(url) {
           // close the current window
           editor.windowManager.close();
-          var item = win.document.getElementById(fieldName);
-          item.value = editor.convertURL(url);
-          // trigger a change event so it will update the text to display
-          if ('createEvent' in document) {
-            var event = document.createEvent('HTMLEvents');
-            event.initEvent('change',!1,!0);
-            item.dispatchEvent(event);
-          } else {
-            item.fireEvent('onchange');
-          }
+          callback(editor.convertURL(url), {text: url});
         }
       });
-    } else if (editor.settings.origFileBrowserCallback) {
-      // call the original file_browser_callback
-      return editor.settings.origFileBrowserCallback(fieldName, url, type, win);
+    } else if (editor.settings.origFilePickerCallback) {
+      // call the original file_picker_callback
+      return editor.settings.origFilePickerCallback(callback, value, meta);
     }
   }
 });
